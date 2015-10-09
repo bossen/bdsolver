9bfecd24ff82d67104963108452dcf70d593250b
Jannek Bossen <jannek.boss@gmail.com>
2015-10-09 14:09:05 +0200

    resolves #35 initcompute() extractrandomfromset() implemented

--- flylikeag6/src/earthmover/bipseudometric.go
+++ flylikeag6/src/earthmover/bipseudometric.go
@@ -18,8 +18,29 @@ func initcoupling() int {
 	return 1
 }
 
-func extractrandomfromset(tocompute *int) (int, int) {
-	return 1, 1
+func initToCompute(n int) *[][]bool {
+  toCompute := *sets.MakeMatrix(n)
+  for i := range toCompute {
+    for j := range toCompute {
+      if i == j {
+        toCompute[i][j] = false
+      } else {
+        toCompute[i][j] = true
+      }
+    }
+  }
+  return &toCompute
+}
+
+func extractrandomfromset(tocompute *[][]bool) (int, int) {
+  for i := range *tocompute {
+    for j:= range *tocompute {
+      if (*tocompute)[i][j] == true && i != j {
+        return i, j
+      }
+    }
+  }
+  return -1, -1
 }
 
 func label(node int) string {
@@ -36,19 +57,25 @@ func getoptimalschedule(d [256][256] int, u int, v int) int {
 	return 1
 }
 
+func isOptimal() bool {
+  return true
+}
 
 
 
 func BipseudoMetric() {
 	var d [256][256]int
+  n := 32
 	visited := initvisited()
 	exact := initexact()
 	coupling := initcoupling()
-	tocompute := 0
+	tocompute := initToCompute(n)
 	lambda := 1
 	
 	for !sets.EmptySet(tocompute) {
-		s, t := extractrandomfromset(&tocompute)
+		s, t := extractrandomfromset(tocompute)
+    fmt.Println(s)
+    fmt.Println(t)
 		if label(s) != label(t) {
 			d[s][t] = 1
 			exact = sets.UnionNode(exact, s, t)
@@ -63,7 +90,7 @@ func BipseudoMetric() {
 			
 			disc(lambda, s, t, &exact, &coupling)
 
-			for  { // TODO: add u, v in for loop
+			for !isOptimal()  { // TODO: add u, v in for loop. While loop check for NOT optimal matching
 				// w := getoptimalschedule(d, u, v)
 				w := 1
 				setpair(1, s, t, w, &exact, &visited, &coupling)
@@ -72,8 +99,8 @@ func BipseudoMetric() {
 			exact = sets.Union(exact, reachable(s, t, coupling))
 			removeedgesfromnodes(&coupling, &exact)
 		}
-		tocompute = sets.Intersect(tocompute, exact)
-
+    
+		tocompute = sets.IntersectReal(*tocompute, *tocompute) //TODO THIS IS WRONG, use exact as second parameter, instead of tocompute twice
 
 	}
 	setpair(1, 1, 1, 1, &exact, &visited, &coupling)
