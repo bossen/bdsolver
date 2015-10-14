2ac9e5e95d1da4a538e6003e0ef5557086583764
smarties89 <smarties89>
2015-10-14 10:48:18 +0200

    Sadpantz codereview notes implemented

--- flylikeag6/src/coupling/coupling.go
+++ flylikeag6/src/coupling/coupling.go
@@ -34,6 +34,16 @@ func Reachable(u, v int, c Coupling) []StatePair {
     // implement using lists instead.
     var reachables []StatePair
 
+    // Adding itself to reachables
+    for _, ce := range c.Matchings[StatePair{u, v}] {
+        if ce.S == u && ce.T == v {
+            ce.Color = 1
+        }
+
+    }
+    reachables = append(reachables, StatePair{u, v})
+
+    // Find all reachables from the  u,v node
     reachables = visit(u, v, c, reachables)
 
     log.Println("reachables:")
