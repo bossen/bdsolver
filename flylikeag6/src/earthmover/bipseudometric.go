75d772bf5dce32626c66fc6883ae9abb22550d60
Jannek Bossen <jannek.boss@gmail.com>
2015-10-16 15:29:25 +0200

    go fmt

--- flylikeag6/src/earthmover/bipseudometric.go
+++ flylikeag6/src/earthmover/bipseudometric.go
@@ -1,51 +1,49 @@
 package earthmover
 
-
 import (
+	"coupling"
 	"log"
-  "sets"
-  "markov"
-  "coupling"
+	"markov"
+	"sets"
 )
 
 func extractrandomfromset(tocompute *[][]bool) (int, int) {
-  for i := range *tocompute {
-    for j:= range *tocompute {
-      if (*tocompute)[i][j] == true {
-        return i, j
-      }
-    }
-  }
-  panic("Tried to extract random element from empty set!")
+	for i := range *tocompute {
+		for j := range *tocompute {
+			if (*tocompute)[i][j] == true {
+				return i, j
+			}
+		}
+	}
+	panic("Tried to extract random element from empty set!")
 }
 
 func removeedgesfromnodes(c *coupling.Coupling, exact *[][]bool) int {
 	return 1
 }
 
-
-func getoptimalschedule(d [256][256] int, u int, v int) int {
+func getoptimalschedule(d [256][256]int, u int, v int) int {
 	return 1
 }
 
 func isOptimal() bool {
-  return true
+	return true
 }
 
 func randommatching(m markov.MarkovChain, u int, v int) [][]float64 {
 	j, k, n := 0, 0, len(m.Labels)
 	uTransitions := make([]float64, n, n)
 	vTransitions := make([]float64, n, n)
-	
+
 	copy(uTransitions, m.Transitions[u])
 	copy(vTransitions, m.Transitions[v])
-	
+
 	matching := make([][]float64, n, n)
-	
+
 	for i := range matching {
 		matching[i] = make([]float64, n, n)
 	}
-	
+
 	for j < n && k < n {
 		if approxFloatEqual(uTransitions[j], vTransitions[k]) {
 			matching[j][k] = uTransitions[j]
@@ -61,15 +59,15 @@ func randommatching(m markov.MarkovChain, u int, v int) [][]float64 {
 			k++
 		}
 	}
-	
+
 	return matching
 }
 
 // credits to https://gist.github.com/cevaris/bc331cbe970b03816c6b
 func approxFloatEqual(a, b float64) bool {
 	var epsilon float64 = 0.00000001
-	
-	if ((a - b) < epsilon && (b - a) < epsilon) {
+
+	if (a-b) < epsilon && (b-a) < epsilon {
 		return true
 	}
 	return false
@@ -79,24 +77,24 @@ func SteppingStone(node *coupling.Node, i int, j int) {
 }
 
 func findNode(s int, t int, c *coupling.Coupling) coupling.Node {
-  newnode := coupling.Node{S:0, T: 0}
-  return newnode
+	newnode := coupling.Node{S: 0, T: 0}
+	return newnode
 }
 
-func BipseudoMetric(m markov.MarkovChain,  lambda int, tocompute *[][]bool) {
+func BipseudoMetric(m markov.MarkovChain, lambda int, tocompute *[][]bool) {
 	var d [256][256]int
-    n := len(m.Transitions)
+	n := len(m.Transitions)
 	visited := *sets.MakeMatrix(n)
 	exact := *sets.MakeMatrix(n)
 	c := coupling.New()
 
 	w2 := randommatching(m, 0, 1)
 	log.Println(w2)
-	
+
 	for !sets.EmptySet(tocompute) {
 		s, t := extractrandomfromset(tocompute)
-    log.Println(s)
-    log.Println(t)
+		log.Println(s)
+		log.Println(t)
 		if m.Labels[s] != m.Labels[t] {
 			d[s][t] = 1
 			exact[s][t] = true
@@ -109,28 +107,28 @@ func BipseudoMetric(m markov.MarkovChain,  lambda int, tocompute *[][]bool) {
 		} else {
 			// if s,t not in visited ...
 
-            disc(lambda, s, t, &exact, &c)
+			disc(lambda, s, t, &exact, &c)
 
-      node := findNode(s, t, &c)
-      minimumvalue := 1.0 //TODO remove when 
-      _ = node
-      //minimumvalue, iindex, jindex := Uvmethod(&node)
-			for (minimumvalue < 0) {
-        //SteppingStone(&node, iindex, jindex)
+			node := findNode(s, t, &c)
+			minimumvalue := 1.0 //TODO remove when
+			_ = node
+			//minimumvalue, iindex, jindex := Uvmethod(&node)
+			for minimumvalue < 0 {
+				//SteppingStone(&node, iindex, jindex)
 
 				// w := getoptimalschedule(d, u, v) TODO this instead of next line
-				w := randommatching(m, s ,t)
+				w := randommatching(m, s, t)
 				setpair(m, s, t, w, &exact, &visited, &c)
 				disc(lambda, s, t, &exact, &c)
-        //minimumvalue, iindex, jindex = Uvmethod(&node)
+				//minimumvalue, iindex, jindex = Uvmethod(&node)
 			}
 			//exact = sets.UnionReal(exact, reachable(s, t, c)) TODO update when reachable has been made
 			removeedgesfromnodes(&c, &exact)
 		}
-    
+
 		tocompute = sets.IntersectReal(*tocompute, *tocompute) //TODO THIS IS WRONG, use exact as second parameter, instead of tocompute twice
 
-    break; //TODO remove this. This is for ending the code
+		break //TODO remove this. This is for ending the code
 	}
 	setpair(m, 1, 1, w2, &exact, &visited, &c)
 	disc(1, 1, 1, &exact, &c)
--- flylikeag6/src/earthmover/uvmethod.go
+++ flylikeag6/src/earthmover/uvmethod.go
@@ -1,76 +1,76 @@
 package earthmover
 
 import (
-  "coupling"
+	"coupling"
 )
 
 func findMinimum(tableu *[][]coupling.Edge, u []float64, v []float64) (float64, int, int) {
-  cols := (*tableu)[0]
+	cols := (*tableu)[0]
 
-  min := (*tableu)[0][0].Prob
-  current := min
-  iresult := 0
-  jresult := 0
+	min := (*tableu)[0][0].Prob
+	current := min
+	iresult := 0
+	jresult := 0
 
-  for i := range *tableu {
-    for j:= range cols {
-      current = (*tableu)[i][j].Prob - u[i] - v[j]
-      if (current < min) {
-        min = current
-        iresult = i
-        jresult = j
-      }
-    }
-  }
-  return min, iresult, jresult
+	for i := range *tableu {
+		for j := range cols {
+			current = (*tableu)[i][j].Prob - u[i] - v[j]
+			if current < min {
+				min = current
+				iresult = i
+				jresult = j
+			}
+		}
+	}
+	return min, iresult, jresult
 }
 
 func calculateuv(tableu *[][]coupling.Edge, u *[]float64, v *[]float64, udefined *[]bool, vdefined *[]bool) {
-  cols := (*tableu)[0]
+	cols := (*tableu)[0]
 
-  first := true
-  for i := range *tableu {
-    for j := range cols {
-      if (!(*tableu)[i][j].IsBasic) {
-        continue
-      }
+	first := true
+	for i := range *tableu {
+		for j := range cols {
+			if !(*tableu)[i][j].IsBasic {
+				continue
+			}
 
-      if (first) {
-        (*u)[i] = 0
-        (*udefined)[i] = true
-        first = false
-      }
+			if first {
+				(*u)[i] = 0
+				(*udefined)[i] = true
+				first = false
+			}
 
-      if ((*udefined)[i]) {
-        (*v)[j] = (*tableu)[i][j].Prob - (*u)[i]
-        (*vdefined)[j] = true
+			if (*udefined)[i] {
+				(*v)[j] = (*tableu)[i][j].Prob - (*u)[i]
+				(*vdefined)[j] = true
 
-      } else if ((*vdefined)[j]) {
-        (*u)[i] = (*tableu)[i][j].Prob - (*v)[j]
-        (*vdefined)[j] = true
-      }
-    }
-  }
+			} else if (*vdefined)[j] {
+				(*u)[i] = (*tableu)[i][j].Prob - (*v)[j]
+				(*vdefined)[j] = true
+			}
+		}
+	}
 }
 
 func Uvmethod(node *coupling.Node) (float64, int, int) {
-  if (node.Adj == nil) {
-    panic("Empty node!")
-  }
+	if node.Adj == nil {
+		panic("Empty node!")
+	}
 
-  tableu := node.Adj
-  cols := (*tableu)[0]
+	tableu := node.Adj
+	cols := (*tableu)[0]
 
-  ulen := len(*tableu)
-  u := make([]float64, ulen, ulen)
-  udefined := make([]bool, ulen, ulen)
+	ulen := len(*tableu)
+	u := make([]float64, ulen, ulen)
+	udefined := make([]bool, ulen, ulen)
 
-  vlen := len(cols)
-  v := make([]float64, vlen, vlen)
-  vdefined := make([]bool, ulen, ulen)
+	vlen := len(cols)
+	v := make([]float64, vlen, vlen)
+	vdefined := make([]bool, ulen, ulen)
 
-  calculateuv(tableu, &u, &v, &udefined, &vdefined)
+	calculateuv(tableu, &u, &v, &udefined, &vdefined)
 
-  min, iindex, jindex := findMinimum(tableu, u, v)
-  return min, iindex, jindex
+	min, iindex, jindex := findMinimum(tableu, u, v)
+	return min, iindex, jindex
 }
