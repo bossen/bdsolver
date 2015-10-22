f445c51160ce00c644cb4cb2c9f1bfcd9326e9ce
sadpants <sadpantz@gmail.com>
2015-10-22 10:08:05 +0200

    changed coupling testing such that it works

--- flylikeag6/src/coupling/coupling_test.go
+++ flylikeag6/src/coupling/coupling_test.go
@@ -14,7 +14,7 @@ func setUpCoupling() Coupling {
     e1 := Edge{&n1, 0.5, true}
     e2 := Edge{&n2, 0.2, true}
     e3 := Edge{&n3, 0, false}
-    e4 := coupling.Edge{&n4, 0.3, true}
+    e4 := Edge{&n4, 0.3, true}
     n2.Adj = [][]*Edge{[]*Edge{&e1, &e2}, []*Edge{&e3, &e4}}
     c.Nodes = []*Node{&n1, &n2, &n3, &n4}
     
@@ -27,7 +27,7 @@ func TestStub(t *testing.T) {
 
 func TestReachableNilAdj(t *testing.T) {
 	c := setUpCoupling()
-    r := coupling.Reachable(0, 0, c)
+    r := Reachable(0, 0, c)
     
     assert.Equal(t, len(r), 1, "size of the reachable set should be 1 if Adj is nil")
 }
@@ -38,7 +38,7 @@ func TestResetsVisitedToFalse(t *testing.T) {
     // set the start to true just to check if it actually is set to false during reachable
     c.Nodes[1].Visited = true
     
-    coupling.Reachable(0, 1, c)
+    Reachable(0, 1, c)
     
     for i := 0; i < 4; i++ {
 		assert.False(t, c.Nodes[i].Visited, "the visited variable date type was not changed back to false")
@@ -48,14 +48,14 @@ func TestResetsVisitedToFalse(t *testing.T) {
 func TestReachableSetCardinality(t *testing.T) {
 	c := setUpCoupling()
     
-    r := coupling.Reachable(0, 1, c)
+    r := Reachable(0, 1, c)
     
     assert.Equal(t, len(r), 3, "the cardinality of the reachable set should be 3")
     
     // test if it correctly finds the correct reachable set after we make one of the nodes unreachable
     c.Nodes[1].Adj[1][1].Prob = 0.0
     
-    r = coupling.Reachable(0, 1, c)
+    r = Reachable(0, 1, c)
     
     assert.Equal(t, len(r), 2, "the cardinality of the reachable set should be 2")
 }
