cc80ccc7a8586bb2c90050b374eccb65dfd49920
sadpants <sadpantz@gmail.com>
2015-11-01 14:46:34 +0100

    final tests for setpair

--- flylikeag6/src/earthmover/setpair.go
+++ flylikeag6/src/earthmover/setpair.go
@@ -7,16 +7,12 @@ import (
 )
 
 func setpair(m markov.MarkovChain, s int, t int, w *coupling.Node, exact [][]bool, visited [][]bool, dist [][]float64, c *coupling.Coupling) {
-	log.Println("s and t in setPair: ", s, t)
+	log.Printf("Setting pair for %v and %v", s, t)
 	visited[s][t] = true
 	visited[t][s] = true
 	
 	for _, rows := range w.Adj {
 		for _, edge := range rows {
-			if approxFloatEqual(0, edge.Prob) {
-				continue
-			}
-			
 			if approxFloatEqual(edge.Prob, 0.0) {
 				continue
 			}
@@ -37,18 +33,18 @@ func setpair(m markov.MarkovChain, s int, t int, w *coupling.Node, exact [][]boo
 			visited[v][u] = true
 			
 			if u == v {
-				log.Println("u and v is the same state ", u ,v)
+				log.Printf("%v and %v is the same state ", u ,v)
 				setdistance(dist, u, v, 0)
 				exact[v][u] = true
 				
 			} else if m.Labels[u] != m.Labels[v] {
-				log.Println("u and v has different labels ", u, v)
+				log.Printf("%v and %v has different labels ", u, v)
 				setdistance(dist, u, v, 1)
 				exact[u][v] = true
 				exact[v][u] = true
 				
 			} else if !(exact[u][v] || exact[v][u]) {
-				log.Println("u and v has the same label ", u, v)
+				log.Printf("%v and %v has the same label ", u, v)
 				w2 := randomMatching(m, u, v, c)
 				setpair(m, u, v, w2, exact, visited, dist, c)
 			}
--- flylikeag6/src/earthmover/setpair_test.go
+++ flylikeag6/src/earthmover/setpair_test.go
@@ -24,6 +24,8 @@ func TestCorrectRecursiveSetPairCall(t *testing.T) {
 	assert.True(t, w.Adj != nil, "the adjacency matrix has not been filled for 0 3")
 	assert.True(t, w.Adj[2][2].To.Adj != nil, "the adjacency matrix has not been filled for 2 3")
 	assert.True(t, w.Adj[0][0].To.Adj == nil, "the adjacency matrix were somehow filled")
+	assert.True(t, w.Adj[0][1].To.Adj == nil, "the adjacency matrix were somehow filled")
+	assert.True(t, w.Adj[1][0].To.Adj == nil, "the adjacency matrix were somehow filled")
 	// checks if the mutual node pointers in the two matchings are the same
 	assert.Equal(t, w.Adj[0][0].To, w.Adj[2][2].To.Adj[0][0].To, "the nodes pointers were not the same")
 	assert.Equal(t, w.Adj[0][1].To, w.Adj[2][2].To.Adj[0][1].To, "the nodes pointers were not the same")
@@ -31,6 +33,66 @@ func TestCorrectRecursiveSetPairCall(t *testing.T) {
 	assert.Equal(t, w.Adj[1][0].To, w.Adj[2][2].To.Adj[1][0].To, "the nodes pointers were not the same")
 	assert.Equal(t, w.Adj[1][1].To, w.Adj[2][2].To.Adj[1][1].To, "the nodes pointers were not the same")
 	assert.Equal(t, w.Adj[1][2].To, w.Adj[2][2].To.Adj[1][2].To, "the nodes pointers were not the same")
+}
+
+func TestCorrectVisited(t *testing.T) {
+	expected := [][]bool{
+		[]bool{false, true, false, true, false, false, false},
+		[]bool{true, true, true, false, false, false, false},
+		[]bool{false, true, true, true, false, true, false},
+		[]bool{true, false, true, false, false, false, false},
+		[]bool{false, false, false, false, false, false, false},
+		[]bool{false, false, true, false, false, false, false},
+		[]bool{false, false, false, false, false, false, false}}
+		
+	c := setUpCoupling()
+	m := setUpMarkov()
+	n := len(m.Transitions)
+	visited := *sets.MakeMatrix(n)
+	exact := *sets.MakeMatrix(n)
+	d := make([][]float64, n, n)
+	for i := 0; i < n; i++ {
+		d[i] = make([]float64, n, n)
+	}
+	
+	w := randomMatching(m, 0, 3, &c)
+	setpair(m, 0, 3, w, exact, visited, d, &c)
+	
+	for i := 0; i < len(expected); i++ {
+		for j := 0; j < len(expected[0]); j++ {
+			assert.Equal(t, expected[i][j], visited[i][j], "the cell in visited were not correcly set")
+		}
+	}
+}
+
+func TestCorrectExact(t *testing.T) {
+	expected := [][]bool{
+		[]bool{false, true, false, false, false, false, false},
+		[]bool{true, true, true, false, false, false, false},
+		[]bool{false, true, true, false, false, true, false},
+		[]bool{false, false, false, false, false, false, false},
+		[]bool{false, false, false, false, false, false, false},
+		[]bool{false, false, true, false, false, false, false},
+		[]bool{false, false, false, false, false, false, false}}
+		
+	c := setUpCoupling()
+	m := setUpMarkov()
+	n := len(m.Transitions)
+	visited := *sets.MakeMatrix(n)
+	exact := *sets.MakeMatrix(n)
+	d := make([][]float64, n, n)
+	for i := 0; i < n; i++ {
+		d[i] = make([]float64, n, n)
+	}
+	
+	w := randomMatching(m, 0, 3, &c)
+	setpair(m, 0, 3, w, exact, visited, d, &c)
+	
+	for i := 0; i < len(expected); i++ {
+		for j := 0; j < len(expected[0]); j++ {
+			assert.Equal(t, expected[i][j], exact[i][j], "the cell in visited were not correcly set")
+		}
+	}
 }
 
 func TestCorrectNestedMatchingFound(t *testing.T) {
