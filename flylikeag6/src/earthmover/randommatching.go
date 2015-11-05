62623b2c58322fa71f4521f8d59c5cfd6ebef5ab
sadpants <sadpantz@gmail.com>
2015-11-05 13:30:11 +0100

    decomposed some of random matching

--- flylikeag6/src/earthmover/randommatching.go
+++ flylikeag6/src/earthmover/randommatching.go
@@ -6,59 +6,40 @@ import (
 	"markov"
 )
 
-func randomMatching(m markov.MarkovChain, u int, v int, c *coupling.Coupling) *coupling.Node {
-	n := len(m.Transitions[u])
+func matchiningDimensions(u, v []float64, n int) (int, int) {
 	lenrow, lencol := 0, 0
-	l, k := 0, 0
-
-	u, v = swapMin(u, v)
-
-	log.Printf("copy the transitions from the states %v and %v", u, v)
-	uTransitions := make([]float64, n, n)
-	vTransitions := make([]float64, n, n)
-
-	copy(uTransitions, m.Transitions[u])
-	copy(vTransitions, m.Transitions[v])
-
-	log.Println(uTransitions)
-	log.Println(vTransitions)
-
-	for i := 0; i < n; i++ {
-		if m.Transitions[u][i] > 0 {
+	
+	for i:= 0; i < n; i++ {
+		if u[i] > 0 {
 			lenrow += 1
 		}
-		if m.Transitions[v][i] > 0 {
+		if v[i] > 0 {
 			lencol += 1
 		}
 	}
-	log.Printf("row and column length are: %v and %v", lenrow, lencol)
+	
+	return lenrow, lencol
+}
 
-	rowindex := make([]int, lenrow, lenrow)
-	colindex := make([]int, lencol, lencol)
-
+func findMatchingIndexes(u, v []float64, n int, row, col []int) {
+	l, k := 0, 0
 	for i := 0; i < n; i++ {
-		if m.Transitions[u][i] > 0 {
-			rowindex[l] = i
+		if u[i] > 0 {
+			row[l] = i
 			l++
 		}
-		if m.Transitions[v][i] > 0 {
-			colindex[k] = i
+		if v[i] > 0 {
+			col[k] = i
 			k++
 		}
 	}
-	log.Printf("row index: %s", rowindex)
-	log.Printf("column index: %s", colindex)
+}
 
-	matching := make([][]*coupling.Edge, lenrow, lenrow)
-
-	for i := range matching {
-		matching[i] = make([]*coupling.Edge, lencol, lencol)
-	}
-
+func filloutAdj(row, col []int, lenrow, lencol int, w [][]*coupling.Edge, c *coupling.Coupling) {
 	for i := 0; i < lenrow; i++ {
 		for j := 0; j < lencol; j++ {
 			var node *coupling.Node
-			s, t := swapMin(rowindex[i], colindex[j])
+			s, t := swapMin(row[i], col[j])
 
 			node = coupling.FindNode(s, t, c)
 
@@ -67,12 +48,52 @@ func randomMatching(m markov.MarkovChain, u int, v int, c *coupling.Coupling) *c
 				c.Nodes = append(c.Nodes, node)
 			}
 
-			matching[i][j] = &coupling.Edge{To: node}
+			w[i][j] = &coupling.Edge{To: node}
 		}
 	}
+}
 
-	l, k = 0, 0
+func randomMatching(m markov.MarkovChain, u int, v int, c *coupling.Coupling) *coupling.Node {
+	n := len(m.Transitions[u])
+	l, k := 0, 0
 
+	u, v = swapMin(u, v)
+
+	log.Printf("copy the transitions from the states %v and %v", u, v)
+	uTransitions := make([]float64, n, n)
+	vTransitions := make([]float64, n, n)
+
+	// copies the transitions of u and v such that we do not makes changes in the markov chain
+	copy(uTransitions, m.Transitions[u])
+	copy(vTransitions, m.Transitions[v])
+
+	log.Println(uTransitions)
+	log.Println(vTransitions)
+	
+	// finds the length of the rows and columns in the matching for u and v
+	lenrow, lencol := matchiningDimensions(uTransitions, vTransitions, n)
+	
+	log.Printf("row and column length are: %v and %v", lenrow, lencol)
+
+	rowindex := make([]int, lenrow, lenrow)
+	colindex := make([]int, lencol, lencol)
+	
+	// finds the row and column indexs for the u and v matching
+	findMatchingIndexes(uTransitions, vTransitions, n, rowindex, colindex)
+	
+	log.Printf("row index: %s", rowindex)
+	log.Printf("column index: %s", colindex)
+
+	matching := make([][]*coupling.Edge, lenrow, lenrow)
+
+	for i := range matching {
+		matching[i] = make([]*coupling.Edge, lencol, lencol)
+	}
+	
+	// fills out the matching with node pointers, using nodes already in the coupling c if they exist
+	filloutAdj(rowindex, colindex, lenrow, lencol, matching, c)
+
+	// completes the matching by inserting probabilities and setting appropriate cells to basic
 	for l < lenrow && k < lencol {
 		if approxFloatEqual(uTransitions[rowindex[l]], vTransitions[colindex[k]]) {
 			matching[l][k].Prob = uTransitions[rowindex[l]]
