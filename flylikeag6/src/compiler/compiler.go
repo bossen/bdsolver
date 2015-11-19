e5a73ad78a4922e39b06b1f73cfff047f0b52b3b
smarties89 <smarties89>
2015-11-19 12:29:34 +0100

    moved things around. Made the last changes to the readchar/readword/readnumber, now focus is on the actual parsing code

--- flylikeag6/src/compiler/compiler.go
+++ flylikeag6/src/compiler/compiler.go
@@ -1,6 +1,15 @@
 package compiler
 
-import "os"
+import (
+    "os"
+    "log"
+    "strconv"
+)
+
+type token struct {
+    tokentype string
+    val string
+}
 
 
 type Compiler struct {
@@ -17,4 +26,126 @@ func New(filename string) (Compiler, error) {
         false,
         *filelocal,
         0}, err
+}
+
+func (c *Compiler)  maketoken(tokentype, value string) token {
+    validtokentypes := []string {
+        "States",
+        "Edges",
+    }
+
+    for _, vtokens := range validtokentypes {
+        if vtokens == tokentype {
+            return token{tokentype, value}
+        }
+    }
+
+    log.Fatal("Did not find expected keyword, found " + tokentype)
+    return token{}
+}
+
+
+func (c *Compiler) peek() byte {
+    if !c.uselastchar {
+        c.uselastchar = true
+        c.readchar()
+    }
+    return c.lastchar[0]
+}
+
+func (c *Compiler) readchar() byte {
+    if c.uselastchar {
+        c.uselastchar = false
+        return c.lastchar[0]
+    }
+
+    n, _ := c.file.Read(c.lastchar)
+
+    if c.lastchar[0] == '\n' {
+        c.line += 1
+    }
+
+    if n == 0 {
+        log.Fatal("Unexpected end of file")
+    }
+
+    return c.lastchar[0]
+}
+
+func (c *Compiler) endoffile() bool {
+    // TODO Implement
+    return true
+}
+
+func expect(a, b byte) {
+    if a != b {
+        log.Fatal("Expected %s got %s", a, b)
+    }
+}
+
+func (c *Compiler) eatuntil(a byte) {
+
+    for !c.endoffile()  {
+        chr :=  c.readchar()
+        if chr == a {
+            break
+        }
+    }
+    if c.endoffile() {
+        log.Fatal("Unexpected end of file")
+    }
+}
+
+func (c *Compiler) eatwhitespaceandcomments() {
+    for !c.endoffile() {
+        if c.peek() == ' ' || c.peek() == '\t' {
+            c.readchar()
+        }
+    }
+
+    if !c.endoffile() && c.peek() == '/' {
+        expect(c.readchar(), '/')
+        c.eatuntil('\n')
+    }
+}
+
+
+func (c *Compiler) readnumber() int {
+    c.eatwhitespaceandcomments()
+
+    number := ""
+    for !c.endoffile() {
+        if !(c.peek() > '0' && c.peek() < '9') {
+            if c.peek() == ' ' || c.peek() == '\t' || c.peek() == '\n' {
+                break
+            } else {
+                log.Fatal("Expected whitespace after number, but got %s", c.peek())
+            }
+        }
+    }
+    if len(number) == 0 {
+        log.Fatal("Did not read any number")
+    }
+
+    numbasint, err := strconv.Atoi(number)
+    if err != nil {
+        log.Fatal("Could not convert to number. Something really bad happened")
+    }
+
+    return numbasint
+}
+
+func (c *Compiler) readword() string {
+    c.eatwhitespaceandcomments()
+    word := ""
+    for !c.endoffile() {
+        if (c.peek() > 'A' &&  c.peek() < 'Z') || (c.peek() > 'a' && c.peek() < 'z') {
+            word += string(c.readchar())
+        } else if c.peek() == ' ' || c.peek() == '\t' || c.peek() ==  '\n' {
+            break
+        } else {
+            log.Fatal("Unexpected %s", c.peek())
+        }
+    }
+    return word
 }
--- flylikeag6/src/compiler/parse.go
+++ flylikeag6/src/compiler/parse.go
@@ -5,32 +5,11 @@ import (
 )
 
 
-type token struct {
-    tokentype string
-    val string
-}
-
-
-func (c *Compiler)  maketoken(tokentype, value string) token {
-    validtokentypes := []string {
-        "States",
-        "Edges",
-    }
-
-    for _, vtokens := range validtokentypes {
-        if vtokens == tokentype {
-            return token{tokentype, value}
-        }
-    }
-
-    log.Fatal("Did not find expected keyword, found " + tokentype)
-    return token{}
-}
-
-
 func (c *Compiler) scanner() token {
     // TODO clear whitespace
     if c.peek() == 'S' { // Must be States
+        return c.maketoken(c.readword(), "")
+    } else if c.peek() == 'E' {
         return c.maketoken(c.readword(), "")
     } else {
         log.Fatal("Unexpected keyword ", c.peek())
@@ -39,23 +18,25 @@ func (c *Compiler) scanner() token {
 }
 
 
-
 func (c *Compiler) Parse() {
-
-
     state := 0
     for true {
         token := c.scanner()
-        _ = token 
 
         if state == 0 { // We are before States:
+            if token.tokentype != "States" {
+                log.Fatal("Expected States found ", token.tokentype)
+            }
 
-        } else if state == 1 {
+        } else if state == 1 { // We are in States
 
-        } else if state == 2 {
+        } else if state == 2 {  // We are in Edges
 
         } else {
             log.Fatal("Something went wrong while parsing")
         }
+
+
+        break
     }
 }
