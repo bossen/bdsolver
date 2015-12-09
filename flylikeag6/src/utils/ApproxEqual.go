0fc3aadbaed9584f39e60ba11019a25ccde82e1b
smarties89 <smarties89>
2015-12-09 12:40:51 +0100

    Bughunting is started

--- flylikeag6/src/utils/ApproxEqual.go
+++ flylikeag6/src/utils/ApproxEqual.go
@@ -4,7 +4,7 @@ package utils
 
 // credits to https://gist.github.com/cevaris/bc331cbe970b03816c6b
 func ApproxEqual(a, b float64) bool {
-	var epsilon float64 = 0.00000001
+	var epsilon float64 = 0.00000000000001
 
 	if (a-b) < epsilon && (b-a) < epsilon {
 		return true
