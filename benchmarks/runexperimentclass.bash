0fa832a021e440704cfc69da230326258960222e
smarties89 <smarties89>
2015-12-17 18:02:14 +0100

    runexperiments.bash now in lexical order

--- benchmarks/runexperimentclass.bash
+++ benchmarks/runexperimentclass.bash
@@ -2,7 +2,7 @@
 
 export foldertotest=$1
 
-for file in $(find $foldertotest -type f -name "*.lmc")
+for file in $(find $foldertotest -type f -name "*.lmc" | sort)
 do
 	echo "Testing $file"
 	/usr/bin/time --format="$file %U %x" ./bdsolver $file > /dev/null  2>> $foldertotest/results.text
