9a7a3ca35c23373db38c5c4485a0c26e289e82d1
smarties89 <smarties89>
2015-12-18 17:17:34 +0100

    runexperiment

--- benchmarks/runexperimentclass.bash
+++ benchmarks/runexperimentclass.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 export foldertotest=$1
+export outputfile=$2
 
 for file in $(find $foldertotest -type f -name "*.lmc" | sort)
 do
 	echo "Testing $file"
-	/usr/bin/time --format="$file %U %x" ./bdsolver $file > /dev/null  2>> $foldertotest/results.text
+	./runexperiment.bash "$file" "$outputfile"
+	# /usr/bin/time --format="$file %U %x" ./bdsolver -tpsolver cplex $file > /dev/null  2>> $foldertotest/results.text
 	sleep 7
 done
