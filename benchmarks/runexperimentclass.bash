bb27a3ca90a5fe9a63dc30863863c94bd75905ac
smarties89 <smarties89>
2015-12-18 12:29:31 +0100

    removed bad tests

--- benchmarks/runexperimentclass.bash
+++ benchmarks/runexperimentclass.bash
@@ -6,4 +6,5 @@ for file in $(find $foldertotest -type f -name "*.lmc" | sort)
 do
 	echo "Testing $file"
 	/usr/bin/time --format="$file %U %x" ./bdsolver $file > /dev/null  2>> $foldertotest/results.text
+	sleep 7
 done
